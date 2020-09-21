af87a0a9271e55a15db626ef57eb3fee9e9a46f0
djmakwana797 <dmakwana979@rku.ac.in>
2020-09-21 22:27:17 +0530

    Design to read assets, read files and write files added.

--- app/src/main/java/com/rku/tutorial09/MainActivity.java
+++ app/src/main/java/com/rku/tutorial09/MainActivity.java
@@ -3,6 +3,7 @@ package com.rku.tutorial09;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void readAssets(View view) {
+    }
+
+    public void readFiles(View view) {
+    }
+
+    public void writeFiles(View view) {
     }
 }
